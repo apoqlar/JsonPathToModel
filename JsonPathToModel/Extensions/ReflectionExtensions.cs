ccc28b803ff6f9de76d2c5022a891cf20f5b88e2
Mimino74 <mimino.arah@gmail.com>
2025-04-28 09:50:29 +0700

    added datetimeoffset with tests

--- JsonPathToModel/Extensions/ReflectionExtensions.cs
+++ JsonPathToModel/Extensions/ReflectionExtensions.cs
@@ -18,8 +18,11 @@ public static class ReflectionExtensions
     {
         var t = type;
 
-        var result = t.IsPrimitive || t == typeof(decimal) || t == typeof(decimal?) || t == typeof(string) || t == typeof(DateTime)
-            || t == typeof(DateTime?) || t == typeof(DateOnly) || t == typeof(DateOnly?) || t == typeof(byte[])
+        var result = t.IsPrimitive || t == typeof(decimal) || t == typeof(decimal?) || t == typeof(string) 
+            || t == typeof(DateTime) || t == typeof(DateTime?) 
+            || t == typeof(DateOnly) || t == typeof(DateOnly?)
+            || t == typeof(DateTimeOffset) || t == typeof(DateTimeOffset?) 
+            || t == typeof(byte[])
             || t == typeof(int?) || t == typeof(float?) || t == typeof(double?) || t == typeof(long?)
             || t.IsEnum || Nullable.GetUnderlyingType(t)?.IsEnum == true;
 
@@ -29,7 +32,9 @@ public static class ReflectionExtensions
     public static bool IsBoxable(this Type t)
     {
         return t.IsPrimitive || t == typeof(decimal) || t == typeof(DateTime) || t == typeof(DateTime?)
-            || t == typeof(DateOnly) || t == typeof(DateOnly?) || t.IsEnum || Nullable.GetUnderlyingType(t)?.IsEnum == true
+            || t == typeof(DateOnly) || t == typeof(DateOnly?)
+            || t == typeof(DateTimeOffset) || t == typeof(DateTimeOffset?)
+            || t.IsEnum || Nullable.GetUnderlyingType(t)?.IsEnum == true
             || Nullable.GetUnderlyingType(t) != null;
     }
 }
---
+++ tests/JsonPathToModel.Tests/ModelData/SampleModelDates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPathToModel.Tests.ModelData;
+
+public class SampleModelDates
+{
+    public string Id { get; set; } 
+    public SampleModelDatesNested? Nested { get; set; } = new();
+    public List<SampleModelDatesNested>? NestedList { get; set; } = [];
+    public Dictionary<string, SampleModelDatesNested>? NestedDictionary { get; set; } = [];
+}
+
+public class SampleModelDatesNested
+{
+    public DateTimeOffset Date { get; set; }
+    public DateTimeOffset? DateNullable { get; set; }
+    public DateOnly DateOnly { get; set; }
+    public DateOnly? DateOnlyNullable { get; set; }
+}
